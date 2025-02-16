a1f167888348bbedebe55f34f5d7e47190085c19
enochrx <drreayy@gmail.com>
2025-02-16 01:49:56 +0100

    map marker and events

--- script.js
+++ script.js
@@ -11,49 +11,78 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// if (navigator.geolocation)
-navigator.geolocation?.getCurrentPosition(
-  function (position) {
-    //success cb
-    // console.log(position);
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+let map, mapEvent;
 
-    const coords = [latitude, longitude];
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //success cb
+      // console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-    const map = L.map("map").setView(coords, 13);
+      const coords = [latitude, longitude];
 
-    // var Stadia_AlidadeSatellite =
+      map = L.map("map").setView(coords, 13);
 
-    L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}",
-      {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution:
-          '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: "jpg",
-      }
-    ).addTo(map);
+      // var Stadia_AlidadeSatellite =
 
-    // L.tileLayer(
-    //   "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png",
-    //   {
-    //     attribution:
-    //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    //   }
-    // ).addTo(map);
+      L.tileLayer(
+        "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}",
+        {
+          minZoom: 0,
+          maxZoom: 20,
+          attribution:
+            '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          ext: "jpg",
+        }
+      ).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-      .openPopup();
+      // L.tileLayer(
+      //   "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png",
+      //   {
+      //     attribution:
+      //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      //   }
+      // ).addTo(map);
 
-    map.on("click", mapEvent => console.log(mapEvent));
-  },
-  function () {
-    //err cb
-    alert("Can not get your location");
-  }
-);
+      // L.marker(coords)
+      //   .addTo(map)
+      //   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      //   .openPopup();
+
+      //handling click on map
+      map.on("click", mapE => {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
+    },
+    function () {
+      //err cb
+      alert("Can not get your location");
+    }
+  );
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  //Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        // content: "Workout",
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
